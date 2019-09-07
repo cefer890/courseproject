5fd60598b66488d3da85aada135419cd26c85a69
Cefer <cefer.h2018@mail.ru>
2019-09-07 20:10:01 +0400

    Class base views

--- core/urls.py
+++ core/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
      path('xiomi/<int:xiomi_id>',views.xiomi_det, name='xiomi_det'),
      path('news/<int:news_id>',views.news_det, name='news_det'),
      path('reg/', views.reg_user, name='users_reg'),
-     path('sides/', views.sides, name='sides_list'),
-     path('sides/<int:sides_id>', views.sides_det, name='sides_det'),
+     path('sides/', views.SidesList.as_view(), name='sides_list'),
+     path('sides/<int:pk>', views.SidesDetail.as_view(), name='sides_det'),
      path('sides_upload/', views.sides_user, name='sides_upload'),
      path('sides_delete/<int:sides_id>', views.sides_delete, name='sides_delete'),
+     path('sides_edit/<int:sides_id>', views.sides_update, name='sides_update'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import DetailView
+from django.views.generic import DetailView, FormView, ListView
+from django.views.generic.base import View, TemplateView
 
 from core.forms import UserForms, SidesForms
 from core.models import Blog, Xiomi, News, Sides
@@ -11,7 +13,7 @@ from core.models import Blog, Xiomi, News, Sides
 
 def home(request):
     blog = Blog.objects.first()
-    return HttpResponse(blog.desc)
+    return HttpResponse(blog)
 
 
 def xiomi(request):
@@ -40,13 +42,18 @@ from datetime import datetime
 
 def sides(request):
     sides = Sides.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(sides, 10)
+    try:
+        users = paginator.page(page)
+    except PageNotAnInteger:
+        users = paginator.page(1)
+    except EmptyPage:
+        users = paginator.page(paginator.num_pages)
     last_sides = Sides.objects.last()
     embahome = Sides.objects.filter(created_at__lte=datetime.now())
-    return render(request,'sides/sides_list.html',{'sides_list':sides, 'last_sides': last_sides, 'embahome': embahome})
-
-def sides_det(request,sides_id):
-    sides_id = get_object_or_404(Sides, id=sides_id)
-    return render(request,'sides/sides_det.html',{'sides_det':sides_id})
+    return render(request,'sides/sides_list.html',{'sides_list':users, 'last_sides': last_sides, 'embahome': embahome})
 
 def sides_user(request):
 
@@ -56,6 +63,7 @@ def sides_user(request):
             form.save(commit=False)
             form.image = request.FILES['image']
             form.save()
+            return HttpResponseRedirect(reverse('sides_upload'))
     else:
         form = SidesForms()
 
@@ -68,4 +76,46 @@ def sides_delete(request, sides_id):
         sides.delete()
     return HttpResponseRedirect(reverse('sides_list'))
 
+def sides_update(request, sides_id):
+    sides_id = get_object_or_404(Sides, id=sides_id)
+    form = SidesForms(request.POST or None, request.FILES or None, instance=sides_id)
+    if form.is_valid() and request.POST:
+        form.save(commit=False)
+        if 'image' in request.FILES:
+            form.image = request.FILES['image']
+        form.save()
+    return render(request, 'sides/sides_upload.html', {'form': form})
 
+
+class DataJSONSender(View):
+    pass
+
+class Template(TemplateView):
+    template_name = 'sides/sides_upload.html'
+
+
+class Form(FormView):
+    form_class = SidesForms()
+    template_name = ''
+
+class SidesList(ListView):
+    template_name = 'sides/sides_list.html'
+    model = Sides
+    context_object_name = 'sides_list'
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        ctx = super().get_context_data()
+        last_sides = Sides.objects.last()
+        ctx['last_sides'] = last_sides
+        return ctx
+
+class SidesDetail(DetailView):
+    template_name = 'sides/sides_det.html'
+    model = Sides
+    context_object_name = 'sides_det'
+
+
+# def sides_det(request,sides_id):
+#     sides_id = get_object_or_404(Sides, id=sides_id)
+#     return render(request,'sides/sides_det.html',{'sides_det':sides_id})
